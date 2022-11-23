222882332db4ae863f36e1a65af272cd6de376f3
jihoon <eezzee@reconers.com>
2022-11-24 07:56:58 +0900

    [ADD] scss mixins,utilites 파일 추가

--- pages/scss-test/index.js
+++ pages/scss-test/index.js
@@ -5,7 +5,7 @@ export default function ScssTestPage() {
     <>
       <div className="area">
         <ul className="flex-row gutter-10">
-          <li className="col-12 col-6-lg mt-0 mt-35-lg font-dotum">아이템1</li>
+          <li className="col-12 col-6-lg mt-0 mt-35-lg font-dotum ell-3">아이템1아이템1 아이템1 아이템1아이템1아이템1아이템1아이템1아이템1아이템1아이템1아 이템1 아 이템1 아이템1 아 이템1아 이템1아 이템1아이템1아이템1 아이템1아이템1아이템1</li>
           <li className="col-12 col-6-lg">아이템2</li>
           <li className="col-12 col-6-lg">아이템3</li>
           <li className="col-12 col-6-lg">아이템4</li>
